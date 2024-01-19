ea43c2fda9342b1ccf4a3ffeb883f025926dcd40
jade <jade.dasilva.lima.jds@gmail.com>
2024-01-19 15:07:23 -0500

    updated repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ec-portfolio/",
+  base: "/ecportfolio/",
   plugins: [
     vue(),
   ],
