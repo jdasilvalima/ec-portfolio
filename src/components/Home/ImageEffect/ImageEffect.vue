dcbd46ac17b7bb702a67263b9fbe7f07fa86b410
jade <jade.dasilva.lima.jds@gmail.com>
2024-02-04 12:09:08 -0500

    test image effect

---
+++ src/components/Home/ImageEffect/ImageEffect.vue
@@ -0,0 +1,11 @@
+<script setup>
+// https://tympanus.net/codrops/2020/03/17/create-a-wave-motion-effect-on-an-image-with-three-js/
+// Magnetic : https://tympanus.net/codrops/2020/08/05/magnetic-buttons/
+// https://github.com/conorbailey90/rgb-split-distortion-scroll-effect/blob/main/js/app.js
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
